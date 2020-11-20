c0f21ca5c022e3c4b09176b86fc1c3a0bc862349
james <jamesengleback@hotmail.co.uk>
2020-11-20 11:02:39 +0000

    added todo, tinkered on bmg.py

--- mib/bmg.py
+++ mib/bmg.py
@@ -1,9 +1,16 @@
+import re
 import pandas as pd
 
 def clean(path):
-    df = pd.read_csv(path).iloc[6:,:]
-    df = df.drop(6)
-
+    df_orig = pd.read_csv(path)
+    df = df_orig
+    nacounts = [sum(df.iloc[i,:].isna()) for i in df.index] # use to locate metadata
+    print(nacounts)
+    # retain metadata?
+    # metadata rows have lots of NaN
+    # use regex to find well IDs (A01, ..) & time points (0h15,..)
+    # if timepoints & 1 wavelenth - idx = wells, headers = time
+    # if end point, idx = wells, headers = wavelength
     return df
 
 def test():
---
+++ todo.md
@@ -0,0 +1,7 @@
+# to do
+
+# bmg plate readers (bmg.py)
+
+# uv-vis 
+
+# open source license
